301f69a1936e87c756e75a3465012d985b62089c
Denis Galtsev <galtsevdv@gmail.com>
2018-05-26 21:55:39 +0300

    initial

---
+++ Graphql/Parser.elm
@@ -0,0 +1,117 @@
+module Graphql.Parser exposing (..)
+
+import Parser exposing (Parser, Count(..), (|.), (|=), repeat, ignore, symbol,
+     oneOf, succeed, oneOrMore, zeroOrMore, source, fail, andThen, delayedCommit)
+import Char
+
+type Type = 
+    IntType
+    | StringType
+    | FloatType
+    | Required Type
+    | ListOf Type
+    | Ref Ref
+
+type alias Ref = String
+
+type alias Field = {name: String, typ: Type}
+
+type alias RecordType = {name: String, fields: List Field}
+type alias InterfaceType = {name: String, fields: List Field}
+
+type Definition = RecordDef RecordType | InterfaceDef InterfaceType
+
+parser : Parser (List Definition)
+parser =
+    succeed identity
+        |. sp
+        |= sequence definitionParser
+
+definitionParser : Parser Definition
+definitionParser =
+    oneOf
+        [ recordTypeParser
+        , interfaceTypeParser
+        ]
+
+makeRecordDef : String -> List Field -> Definition
+makeRecordDef name fields = RecordDef {name = name, fields = fields}
+
+recordTypeParser : Parser Definition
+recordTypeParser =
+    succeed makeRecordDef
+        |. symbol "type"
+        |. space
+        |= ident
+        |. sp
+        |. symbol "{"
+        |. sp
+        |= sequence fieldParser
+        |. sp
+        |. symbol "}"
+
+interfaceTypeParser : Parser Definition
+interfaceTypeParser =
+    succeed (InterfaceDef {name = "test", fields=[]})
+
+sequence : Parser a -> Parser (List a)
+sequence parser =
+    let
+        step =
+            delayedCommit space <|
+                succeed identity
+                    |= parser
+    in
+    succeed (::)
+        |= parser
+        |= repeat zeroOrMore step
+
+alpha : Char -> Bool
+alpha c = Char.isLower c || Char.isUpper c
+
+identChar : Char -> Bool
+identChar c = Char.isDigit c || alpha c || c=='_'
+
+makeField : String -> Type -> Field
+makeField name typ = {name = name, typ = typ}
+
+fieldParser : Parser Field
+fieldParser =
+    succeed makeField
+        |= ident
+        |. sp
+        |. symbol ":"
+        |. sp
+        |= typeParser
+
+parseType : String -> Result String Type
+parseType name =
+    case name of
+        "int" -> Ok IntType
+        "string" -> Ok StringType
+        "float" -> Ok FloatType
+        _ -> Err <| "bad type name:" ++ name
+
+typeParser : Parser Type
+typeParser =
+    ident
+        |> Parser.map parseType
+        |> andThen (\name ->
+            case name of
+                Ok t -> succeed t
+                Err err -> fail err
+            )
+
+ident : Parser String
+ident =
+    source <|
+        ignore (Exactly 1) alpha
+            |. ignore zeroOrMore identChar
+
+sp : Parser ()
+sp =
+    ignore zeroOrMore (\c -> c==' ' || c=='\n' || c=='\t')
+
+space : Parser ()
+space =
+    ignore oneOrMore (\c -> c==' ' || c=='\n' || c=='\t')
---
+++ tests/Example.elm
@@ -0,0 +1,97 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+import Graphql.Parser exposing (..)
+import Parser exposing (run)
+
+isOk : Result a b -> Bool
+isOk r =
+    case r of
+        Ok _ -> True
+        Err _ -> False
+
+expectOk : Result x a -> Expectation
+expectOk res =
+    case res of
+        Ok _ -> Expect.pass
+        Err err -> Expect.fail <| toString err
+
+expectType : Type -> Result x Field -> Expectation
+expectType etyp res =
+    case res of
+        Ok {typ} ->
+            if typ==etyp 
+                then Expect.pass
+                else Expect.fail <| "type mismatch" ++ toString typ
+        Err err -> Expect.fail <| toString err
+
+suite : Test
+suite =
+    describe "parser"
+        [ test "fieldParser int" <|
+            \_ ->
+                run fieldParser "id: int"
+                    |> expectType IntType
+        , test "fieldParser float" <|
+            \_ ->
+                run fieldParser "weigth :float"
+                    |> expectType FloatType
+        , test "fieldParser string" <|
+            \_ ->
+                run fieldParser "name : string"
+                    |> expectType StringType
+        , test "recordTypeParser" <|
+            \_ ->
+                """type MyType {id: int name: string }
+                """
+                    |> run recordTypeParser
+                    |> expectOk
+        , test "recordTypeParser multiline" <|
+            \_ ->
+                """type MyType {
+                    id: int
+                    name: string
+                }
+                """
+                    |> run recordTypeParser
+                    |> expectOk
+        , test "document with two types" <|
+            \_ ->
+                let
+                    extractDef def =
+                        case def of
+                            RecordDef {name,fields} -> List.map (\f->name++"."++f.name) fields
+                            InterfaceDef _ -> []
+                    extractFields defs =
+                        case defs of
+                            Err err -> []
+                            Ok defs ->
+                                List.map extractDef defs
+                                    |> List.concat
+                in
+                """
+                type Post {
+                    id: int
+                    title: string
+                    stars: int
+                }
+                type Comment {
+                    id: int
+                    user: string
+                    post_id: int
+                }
+                """
+                    |> run parser
+                    |> expectOk
+                    -- |> extractFields
+                    -- |> Expect.equalLists 
+                    --     [ "Post.id"
+                    --     , "Post.title"
+                    --     , "Post.stars"
+                    --     , "Comment.id"
+                    --     , "Comment.user"
+                    --     , "Comment.post_id"
+                    --     ]
+        ]
